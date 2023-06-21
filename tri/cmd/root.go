133aaaf7a3eb1ab2d5b527dba585ac4f33ee6bdf
andersonbosa <andersonbosa0@gmail.com>
2023-06-20 21:47:12 -0300

    release 1.0.0
    * add config file example (yml)
    * add todo list example (json)
    * update config file extension on root.go

--- tri/cmd/root.go
+++ tri/cmd/root.go
@@ -45,7 +45,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tri.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tri.yml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
